06440b3b9b15a95153fe15bf3d1c1c9626ed2405
Eddie Orlando <2059713180109@ingenieria.usac.edu.gt>
2020-09-21 02:27:12 -0600

    feature-gramatica. ide.js - Se delegaron las funciones del front a este archivo. ide.html - Se agrego la grafica de un arbol cualquiera para prueba.

---
+++ src/scripts/front/ide.js
@@ -0,0 +1,28 @@
+const CONSOLE_LINE_MARK = '->';
+const CONSOLE_MESSAGE_SUCCESSFULL="OK";
+const ENTER = "\n";
+
+var _backEnd;
+var _txtConsola;
+
+/**
+ * Metodo que ejecuta la entrada
+ * @param entrada
+ */
+const ejecutar= function (cadEntrada){
+    try {
+        print("Iniciando ejecucion: "+new Date());
+        _backEnd = new BackEnd();
+        let root = analyzer.parse(cadEntrada);
+        //console.log(new Date());
+        print(CONSOLE_MESSAGE_SUCCESSFULL);
+    }catch (e){
+        alert(e);
+    }
+}
+
+
+const print = function (strTexto){
+    let strCad=_txtConsola.val();
+    _txtConsola.val(strCad+strTexto+ENTER+CONSOLE_LINE_MARK);
+}
